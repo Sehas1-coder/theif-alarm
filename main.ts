8e4f0b3031aae0a19af8dfa552675a25e67ee989
Sehas1-coder <thanudaya.uk@gmail.com>
2025-07-19 13:12:25 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ input.onButtonPressed(Button.A, function () {
     music.stopAllSounds()
 })
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) > 500 || pins.analogReadPin(AnalogReadWritePin.P2) == 1) {
+    let motion_sensor = 0
+    if (pins.analogReadPin(AnalogPin.P0) == motion_sensor) {
         basic.showIcon(IconNames.Surprised)
         music.play(music.createSoundExpression(WaveShape.Sawtooth, 3003, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.LoopingInBackground)
     } else {
